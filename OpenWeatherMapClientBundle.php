e92c47a9a5748d618acfbea6c7a0940da88c3c65
aurimasb <a.berskys@gmail.com>
2018-09-09 10:44:13 +0300

    Change bundle name to a better one

--- OpenWeatherMapClientBundle.php
+++ OpenWeatherMapClientBundle.php
@@ -5,6 +5,6 @@ namespace OpenWeatherMapBundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class OpenWeatherMapBundle extends Bundle
+class OpenWeatherMapClientBundle extends Bundle
 {
 }
